536f464f12c7b43398609385a72e55f75c3a9e87
dharshi <dharshibalasubramaniyam@users.noreply.github.com>
2025-03-26 11:09:09 +0530

    Add comments in server.js

--- workspaces/server/server.js
+++ workspaces/server/server.js
@@ -1,11 +1,12 @@
 const { open } = require('@vscode/test-web');
 
 open({
-    quality: "insiders",
+    quality: "insiders", // For production mode use, quality: "stable"
     browserType: "none",
     // Currently serving the web extension in development mode. 
-    // For production mode use, extensionPaths: ["workspaces/ballerina/ballerina-extension"]
     extensionDevelopmentPath: "workspaces/ballerina/ballerina-extension",
+    // For production mode use, extensionPaths: ["workspaces/ballerina/ballerina-extension"]
+    // extensionPaths: ["workspaces/ballerina/ballerina-extension"],
     port: 3001,
 }).then(() => {
     console.log("done!")
